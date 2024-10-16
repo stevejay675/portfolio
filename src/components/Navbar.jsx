fe6709564dc20e697758b3b1ca8189ba196a0680
stevejay675 <stevejay675@gmail.com>
2024-10-16 04:51:24 +0100

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import { FaCode, FaMoon, FaSun, FaBars } from "react-icons/fa";
+import { useState, useEffect } from 'react';
+
 
 
 const Navbar = () => {
+
+  const [position, setPosition] = useState('absolute');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 150) {
+        setPosition('fixed');
+      } else {
+        setPosition('absolute');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
   return (
-    <div className='navbar'>
+    <div className={`navbar ${[position]}`}>
       <a href='#' className="logo">
       <h2 className='bold'>Steve </h2>
       <span><FaCode className='icon'/></span>
