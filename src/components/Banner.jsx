b0345ad26452f2ac0a7d85c3d4f54668b3a3cf32
stevejay675 <stevejay675@gmail.com>
2024-12-31 02:37:28 +0100

    updates

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import me from '../assets/Me1.jpg';
-import { FaGithub, FaLinkedin, FaLinkedinIn  } from 'react-icons/fa';
+import { FaGithub} from 'react-icons/fa';
 import { TiSocialLinkedinCircular } from "react-icons/ti";
-import cv from '../assets/cv.pdf';
+import cv from '../assets/MyCV.pdf';
 
 const Banner = () => {
   return (
